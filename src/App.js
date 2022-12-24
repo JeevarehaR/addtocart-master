972ae640f62e985b81a1a1db3aac35d4e26c4a7b
JeevarehaR <jeev.reh@gmail.com>
2022-12-24 12:06:30 +0530

    Addtocart added

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import * as React from "react";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { NavigationBar } from "./components/NavigationBar";
+import { Cards } from "./components/Cards";
+import data from "./data/data.json";
+import { useState } from "react";
+
+function App() {
+  const [itemCount, setItemCount] = useState(0);
+  const manageItems = (event) => {
+    if (event.target.innerText === "Add to cart") {
+      addItem(event);
+    } else if (event.target.innerText === "Remove from cart") {
+      removeItem(event);
+    }
+  };
+
+  //add item function to increase item count
+  const addItem = (event) => {
+    setItemCount(() => itemCount + 1);
+    event.target.innerText = "Remove from cart";
+    event.target.classList.value = "btn btn-outline-danger mt-auto";
+  };
+
+  //remove item function to decrease item count
+  const removeItem = (event) => {
+    setItemCount(() => itemCount - 1);
+    event.target.innerText = "Add to cart";
+    event.target.classList.value = "btn btn-outline-dark mt-auto";
+  };
+
+  return (
+    <>
+      <div className="App">
+        <NavigationBar itemCount={itemCount} />
+        <Header />
+        <Cards data={data} manageItems={manageItems} />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+export function Footer() {
+  return (
+    <div className="footer-container">
+      <footer class="py-5 bg-dark">
+        <div class="container">
+          <p class="m-0 text-center text-white">
+            Copyright &copy; Your Website {new Date().getFullYear()}
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
